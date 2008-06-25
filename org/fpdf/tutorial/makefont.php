0f53b9daa3413acd2af30e1d5f447f772329b496
christian.wuerker <christian.wuerker@17e82a80-5050-0410-bae8-e795ea07cb8d>
2008-06-25 11:31:54 +0000

    Folder moved.

---
+++ org/fpdf/tutorial/makefont.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *	@package		fpdf
+ */
+//Generation of font definition file for tutorial 7
+require('../font/makefont/makefont.php');
+
+MakeFont('calligra.ttf','calligra.afm');
+?>
---
+++ org/fpdf/tutorial/tuto1.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *	@package		fpdf
+ */
+require('../fpdf.php');
+
+$pdf=new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,'Hello World!');
+$pdf->Output();
+?>
